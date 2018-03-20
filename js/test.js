54b52a527f7895382ed46fff77e5ba665b1dc659
shadowczp <czp5153409@qq.com>
2018-03-20 18:07:26 +0800

    s

--- js/test.js
+++ js/test.js
@@ -1,8 +1,18 @@
-console.log(undefined === undefined)
-var a = {name: 'czp', age: 20}
-var b = a
-b.age=15
-console.log(a)
-console.log(b)
-var arr1 = ['a','b','c']
-var arr2 = [1,2,3]
+function doThisLater() {
+    alert("haha")
+}
+
+function click1() {
+    //这里返回一个计时器ID
+    var timeId = setTimeout(doThisLater, 2000)
+    //可以清除计时器阻止执行
+    //clearTimeout(timeId)
+}
+
+window.onload = function (ev) {
+    var div1 = document.getElementById('div1')
+    div1.ondragenter=function (ev2) {
+        ev2.target.innerHTML='dd'
+    }
+}
+
