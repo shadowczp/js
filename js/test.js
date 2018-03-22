d12158bac918dc790dfe39d4872bbdf5bb627d49
shadowczp <czp5153409@qq.com>
2018-03-22 17:34:56 +0800

    s

--- js/test.js
+++ js/test.js
@@ -1,20 +1,30 @@
 function doThisLater() {
     alert("haha")
 }
-function test(num1,num2) {
-    var arr=[1,2,3,4,5,6]
-    alert(arr.reduce(function (a,b) {
-        return a+b
-    }))
+
+function test(num1, num2) {
+
 }
+
 function click1() {
-    localStorage.userName='岑志鹏'
-    document.getElementById("name").setAttribute("value",localStorage.userName)
+    localStorage.userName = '岑志鹏'
+    document.getElementById("name").setAttribute("value", localStorage.userName)
 }
 
 window.onload = function (ev) {
-    document.getElementById("name").setAttribute("value",localStorage.userName)
-    test(10)
+    document.getElementById("name").setAttribute("value", localStorage.userName)
+    function Person() {
+    }
+    Person.prototype.msg={name:'czp',age:22}
+    var czp = new Person()
+    var hhh = new Person()
+    hhh.msg.name='hhh'
+    hhh.msg='test'
+    delete hhh.msg
+    alert(JSON.stringify(czp.msg))
+    alert(JSON.stringify(hhh.msg))
+    a='dasda'
+    alert(top.a)
 }
 
 
